44d19ab1bc1fdf6ba2cf0c73dffb273ccf73db41
Jan Drábek <JD-mac@JD-mac.local>
2017-05-04 15:49:54 +0200

    + Toolbar UI.

--- app/src/main/java/cz/drabek/feedreader/articles/ArticlesActivity.java
+++ app/src/main/java/cz/drabek/feedreader/articles/ArticlesActivity.java
@@ -2,6 +2,8 @@ package cz.drabek.feedreader.articles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import cz.drabek.feedreader.R;
 
@@ -11,5 +13,18 @@ public class ArticlesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.articles_act);
+
+        // Set up the toolbar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+    // Inflate the menu; this adds items to the action bar if it is present.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+
 }
