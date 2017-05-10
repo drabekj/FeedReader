685bf16081b383af425c051e7c4f7d1b35a6858e
Jan Drábek <JD-mac@JD-mac.local>
2017-05-10 02:34:23 +0200

    + Created empty BoodBroadcastReceiver.

---
+++ app/src/main/java/cz/drabek/feedreader/service/BootBroadcastReceiver.java
@@ -0,0 +1,13 @@
+package cz.drabek.feedreader.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
